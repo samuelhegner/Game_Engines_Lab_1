cc3e4a676a55299eb9a016004dda2d781d2c3242
Samuel Hegner <C16439262@mydit.ie>
2018-09-21 16:29:03 +0100

    Lab Push 1

---
+++ Assets/BulletDestroyScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletDestroyScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        timer = 0;
+	}
+
+    float timer;
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+
+        if(timer>=5){
+            Destroy(gameObject);
+        }
+	}
+}
---
+++ Assets/Scripts/TankController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankController : MonoBehaviour {
+
+    public float speed = 2;
+    public float rotationSpeed = 10;
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+    public float bulletForce = 100;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKey(KeyCode.W)){
+            transform.Translate(Vector3.forward * (Time.deltaTime * speed));
+        }
+
+        if(Input.GetKey(KeyCode.S)){
+            transform.Translate(Vector3.back * (Time.deltaTime * speed));
+        }
+
+        if(Input.GetKey(KeyCode.A)){
+            transform.Rotate(new Vector3(0, -Time.deltaTime * rotationSpeed, 0));
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(new Vector3(0, Time.deltaTime * rotationSpeed, 0));
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space)){
+            GameObject bullet =Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+            bullet.GetComponent<Rigidbody>().AddForce(bulletSpawn.forward * bulletForce);
+        }
+
+
+
+    }
+}
