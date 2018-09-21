04766f012b76d95ced43b79b5b59c19ae21c9208
Samuel Hegner <C16439262@mydit.ie>
2018-09-21 16:58:24 +0100

    LAb done (I think)

---
+++ Assets/CreateWallScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateWallScript : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+        for (int i = 0; i < 6; i ++){
+            for (int j = 1; j < 7; j++){
+                GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                cube.transform.position = new Vector3(i, j, 0);
+                cube.AddComponent<Rigidbody>();
+
+                cube.GetComponent<MeshRenderer>().material.color = Random.ColorHSV(0, 1f, 1f, 1f, 1f, 1f); ;
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
